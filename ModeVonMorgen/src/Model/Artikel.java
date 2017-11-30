5f1da8337ba0612e080242c6dfb852108212d959
fmaoro <34030589+fmaoro@users.noreply.github.com>
2017-11-30 18:54:10 +0100

    Artikelklassen erstellt

--- ModeVonMorgen/src/Model/Artikel.java
+++ ModeVonMorgen/src/Model/Artikel.java
@@ -1,6 +1,78 @@
 package Model;
 
 public abstract class Artikel {
-//test 
+
+	private int Artikelnummer, Bestand;
+	private String Bezeichnung, Art, Geschlecht, Hersteller, Verfügbarkeit, Notiz;
+	private String[] Lieferanten;
+	private double Preis, Rabatt;
+	
+	public int getArtikelnummer() {
+		return Artikelnummer;
+	}
+	public void setArtikelnummer(int artikelnummer) {
+		Artikelnummer = artikelnummer;
+	}
+	public int getBestand() {
+		return Bestand;
+	}
+	public void setBestand(int bestand) {
+		Bestand = bestand;
+	}
+	public String getBezeichnung() {
+		return Bezeichnung;
+	}
+	public void setBezeichnung(String bezeichnung) {
+		Bezeichnung = bezeichnung;
+	}
+	public String getArt() {
+		return Art;
+	}
+	public void setArt(String art) {
+		Art = art;
+	}
+	public String getGeschlecht() {
+		return Geschlecht;
+	}
+	public void setGeschlecht(String geschlecht) {
+		Geschlecht = geschlecht;
+	}
+	public String getHersteller() {
+		return Hersteller;
+	}
+	public void setHersteller(String hersteller) {
+		Hersteller = hersteller;
+	}
+	public String getVerfügbarkeit() {
+		return Verfügbarkeit;
+	}
+	public void setVerfügbarkeit(String verfügbarkeit) {
+		Verfügbarkeit = verfügbarkeit;
+	}
+	public String getNotiz() {
+		return Notiz;
+	}
+	public void setNotiz(String notiz) {
+		Notiz = notiz;
+	}
+	public String[] getLieferanten() {
+		return Lieferanten;
+	}
+	public void setLieferanten(String[] lieferanten) {
+		Lieferanten = lieferanten;
+	}
+	public double getPreis() {
+		return Preis;
+	}
+	public void setPreis(double preis) {
+		Preis = preis;
+	}
+	public double getRabatt() {
+		return Rabatt;
+	}
+	public void setRabatt(double rabatt) {
+		Rabatt = rabatt;
+	}
+	
 	
 }
