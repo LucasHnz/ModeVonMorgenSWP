66f761d767a0c681291c694a0301f7a63b2bcd9c
fmaoro <34030589+fmaoro@users.noreply.github.com>
2017-12-04 22:54:24 +0100

    GUIArtikel

--- ModeVonMorgen/src/MainPackage/GUINeuerArtikelFormular.java
+++ ModeVonMorgen/src/MainPackage/GUINeuerArtikelFormular.java
@@ -1,9 +1,42 @@
 package MainPackage;
-/**
- * 
- * @author maoro
- *
- */
-public class GUINeuerArtikelFormular {
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class GUINeuerArtikelFormular extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUINeuerArtikelFormular frame = new GUINeuerArtikelFormular();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUINeuerArtikelFormular() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
 
 }
